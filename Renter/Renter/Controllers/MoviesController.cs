24b826dbaf85fc1eff596b50c67fa5e49b47e7f6
iandrei250 <prtdace@gmail.com>
2022-02-24 12:06:16 +0100

    added customers and movies with details

--- Renter/Renter/Controllers/MoviesController.cs
+++ Renter/Renter/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using Renter.Models;
+using Renter.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,21 @@ namespace Renter.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Random()
-        {
 
-            var movie = new Movie() { Name = "Shrek"};
-            return View(movie);
+     public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
         }
+
+     private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-e"}
+            };
+        }   
     }
 }
